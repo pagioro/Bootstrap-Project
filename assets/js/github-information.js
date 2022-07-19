e11bafb897680134bdae75a400f4440608d9fd61
pagioro <pagioro@hotmail.com>
2022-07-19 14:41:27 +0000

    finished add github api

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -61,14 +61,17 @@ function fetchGitHubInformation(event) {
                 $("#gh-user-data").html(userInformationHTML(userData));
                 $("#gh-repo-data").html(repoInformationHTML(repoData));
             },
-        function(errorResponse) {
-            if (errorResponse.status === 404) {
-                $("#gh-user-data").html(
+            function(errorResponse) {
+                if (errorResponse.status === 404) {
+                    $("#gh-user-data").html(
                         `<h2>No info found for user ${username}</h2>`);
-            } else {
-                console.log(errorResponse);
-                $("#gh-user-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                } else if (errorResponse.status === 403) {
+                    let resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                    $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+                } else {
+                    console.log(errorResponse);
+                    $("#gh-user-data").html(
+                        `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
                 }
             });
 }
